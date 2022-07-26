0c8108779ea803bb6446083ea111cb896d96048d
dastbek <duyh91@gmail.com>
2022-07-26 16:10:14 +0700

    More note

---
+++ Stats/Statistics for Python/New Concept Map.mm
@@ -0,0 +1,83 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="New Concept Map" FOLDED="false" ID="ID_13026161" CREATED="1658809725811" MODIFIED="1658809725811" STYLE="oval"><hook NAME="MapStyle" background="#000000">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.connection" LAST="false">
+            <node_periodic_level_condition PERIOD="2" REMAINDER="1"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.topic" LAST="false">
+            <node_level_condition VALUE="2" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.subtopic" LAST="false">
+            <node_level_condition VALUE="4" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.subsubtopic" LAST="false">
+            <node_level_condition VALUE="6" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+    </conditional_styles>
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/conceptMap.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1669985490" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1669985490" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Arial" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork">
+<font NAME="Arial" SIZE="8" BOLD="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="sdd" POSITION="right" ID="ID_1702529503" CREATED="1658809727969" MODIFIED="1658809729529">
+<node TEXT="sdsd" ID="ID_84821142" CREATED="1658809729848" MODIFIED="1658809730961">
+<node TEXT="sdsds" ID="ID_1017242297" CREATED="1658809732336" MODIFIED="1658809733225"/>
+</node>
+<node TEXT="sdsd" ID="ID_1475380803" CREATED="1658809735145" MODIFIED="1658809736002"/>
+<node TEXT="sdsd" ID="ID_425191533" CREATED="1658809737120" MODIFIED="1658809737977"/>
+</node>
+</node>
+</map>
--- Stats/Statistics for Python/Stats.mm
+++ Stats/Statistics for Python/Stats.mm
@@ -1,10 +1,9 @@
 <map version="freeplane 1.9.13">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="Sampling Distribution" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1658646880545" VGAP_QUANTITY="2 pt">
+<node TEXT="Stats Basics" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1658807120254">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
-<font BOLD="true"/>
 <hook NAME="MapStyle" background="#f9f9f8" zoom="0.77">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_nord_template.mm" followedTemplateLocation="template:/light_nord_template.mm" followedMapLastTime="1656133210000" fit_to_viewport="false"/>
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm" followedTemplateLocation="template:/light_nord_template.mm" followedMapLastTime="1656133210000"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
@@ -12,7 +11,7 @@
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" MAX_WIDTH="10 cm" MIN_WIDTH="0 cm" VGAP_QUANTITY="2 pt">
 <arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
-<font NAME="Times New Roman" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<font NAME="Times New Roman" SIZE="12" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
 <edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
 <richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
 <richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
@@ -43,7 +42,7 @@
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
-<font SIZE="18"/>
+<font SIZE="18" BOLD="true"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
 <font SIZE="16"/>
@@ -79,11 +78,24 @@
 </stylenode>
 </map_styles>
 </hook>
-<node TEXT="Multiple Sample Statistics drawn from the same population" POSITION="right" ID="ID_1144278209" CREATED="1658646575961" MODIFIED="1658646756852" VGAP_QUANTITY="2 pt" HGAP_QUANTITY="-232.74999 pt" VSHIFT_QUANTITY="-149.25 pt">
-<node TEXT="Create a distribution of sample ==&gt; Make inference about the population" ID="ID_1247467429" CREATED="1658646634786" MODIFIED="1658646756851" HGAP_QUANTITY="-312.24999 pt" VSHIFT_QUANTITY="-141 pt"/>
+<node TEXT="Tests" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_943656482" CREATED="1658806703413" MODIFIED="1658806934687" HGAP_QUANTITY="1474.49996 pt" VSHIFT_QUANTITY="-545.24998 pt">
+<hook NAME="FreeNode"/>
+<node TEXT="Chi-squared test" ID="ID_1412605770" CREATED="1658806711605" MODIFIED="1658806891363" VGAP_QUANTITY="2 pt">
+<node TEXT="Purpose" ID="ID_1152110680" CREATED="1658806811764" MODIFIED="1658806830357" HGAP_QUANTITY="15.5 pt" VSHIFT_QUANTITY="-129 pt"/>
 </node>
-<node TEXT="Key terms" POSITION="left" ID="ID_1090403636" CREATED="1658646767042" MODIFIED="1658646891154" VGAP_QUANTITY="2 pt" HGAP_QUANTITY="61.25 pt" VSHIFT_QUANTITY="-0.75 pt">
-<node ID="ID_1595137902" CREATED="1658646771786" MODIFIED="1658674567684" HGAP_QUANTITY="27.5 pt" VSHIFT_QUANTITY="-21 pt"><richcontent TYPE="NODE">
+</node>
+<node TEXT="Odds" POSITION="right" ID="ID_1118057959" CREATED="1658801476860" MODIFIED="1658806692047" VGAP_QUANTITY="2 pt">
+<node TEXT="\latex \frac{Happening}{Not \; Happening}" ID="ID_418495666" CREATED="1658801487493" MODIFIED="1658801802632">
+<font BOLD="false"/>
+</node>
+<node TEXT="Odds of head when flipping a fair coin is 1:1" ID="ID_1539261525" CREATED="1658801847556" MODIFIED="1658801873190">
+<node TEXT="Compared to 50% probability (Odds # Probs)" ID="ID_1985991478" CREATED="1658802431667" MODIFIED="1658802451460"/>
+</node>
+<node TEXT="Odds Ratio" ID="ID_491837342" CREATED="1658802132331" MODIFIED="1658809656922" VGAP_QUANTITY="2 pt" HGAP_QUANTITY="13.25 pt" VSHIFT_QUANTITY="94.5 pt">
+<node TEXT="Ratio of 2 odds" ID="ID_164638174" CREATED="1658802141268" MODIFIED="1658802151518">
+<node TEXT="\latex \frac{\text{Odds of mutated gene having cancer}}{\text{Odds of unmutated gene having cancer}}" ID="ID_782605418" CREATED="1658802155028" MODIFIED="1658804159691" VGAP_QUANTITY="2 pt">
+<node TEXT="Similar to R-squared, see if X is a good predictor for Y, when compared to not(X). Relationship between 2 things" ID="ID_561437042" CREATED="1658802308075" MODIFIED="1658804818745">
+<node ID="ID_1566955396" CREATED="1658804829527" MODIFIED="1658806847871" VGAP_QUANTITY="2 pt"><richcontent TYPE="NODE">
 
 <html>
   <head>
@@ -91,76 +103,25 @@
   </head>
   <body>
     <p>
-      <b>Sample Statistics</b>: Metric calculated from sample
+      Test if the <b>Odds Ratio</b>&nbsp;is statistically significant
     </p>
   </body>
 </html>
 
 </richcontent>
+<node TEXT="Chi-squared test" ID="ID_1960141433" CREATED="1658804872647" MODIFIED="1658806861333" HGAP_QUANTITY="13.25 pt" VSHIFT_QUANTITY="-8.25 pt">
+<arrowlink DESTINATION="ID_1412605770" STARTINCLINATION="-33 pt;-156 pt;" ENDINCLINATION="10.5 pt;68.25 pt;"/>
 </node>
-<node ID="ID_808958748" CREATED="1658646788449" MODIFIED="1658674570980" HGAP_QUANTITY="53.75 pt" VSHIFT_QUANTITY="-11.25 pt"><richcontent TYPE="NODE">
-
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <b>Data Distribution</b>: frequency distribution of values
-    </p>
-  </body>
-</html>
-
-</richcontent>
-</node>
-<node ID="ID_1928638543" CREATED="1658646804249" MODIFIED="1658674573417" HGAP_QUANTITY="60.5 pt" VSHIFT_QUANTITY="-18 pt"><richcontent TYPE="NODE">
-
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <b>Sampling Distribution:</b>&nbsp;statistics* (mean, std) distribution built from multiple samples
-    </p>
-  </body>
-</html>
-
-</richcontent>
-</node>
-<node ID="ID_298553653" CREATED="1658646816713" MODIFIED="1658674576694" HGAP_QUANTITY="45.5 pt" VSHIFT_QUANTITY="0.75 pt"><richcontent TYPE="NODE">
-
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <b>Central limit theorem</b>: tendency of sampling distribution to take normal form
-    </p>
-  </body>
-</html>
-
-</richcontent>
-</node>
-<node ID="ID_845507695" CREATED="1658646855465" MODIFIED="1658674588202" HGAP_QUANTITY="34.25 pt" VSHIFT_QUANTITY="29.25 pt"><richcontent TYPE="NODE">
-
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <b>Standard Error</b>: STD of sampling distribution mean
-    </p>
-  </body>
-</html>
-
-</richcontent>
+<node TEXT="Fisher&apos;s exact test" ID="ID_1845775281" CREATED="1658804879287" MODIFIED="1658804886921"/>
+<node TEXT="Wald Test" ID="ID_450854329" CREATED="1658804887807" MODIFIED="1658804890904"/>
 </node>
 </node>
-<node TEXT="Bootstrap" POSITION="right" ID="ID_552116519" CREATED="1658674748832" MODIFIED="1658674751611">
-<node TEXT="Collect multiple samples (with replacement) to calculate the sampling distribution of a statistics (mean, std..)" ID="ID_652486965" CREATED="1658674754952" MODIFIED="1658674849706" VGAP_QUANTITY="2 pt"/>
+<node TEXT="Example from Youtube" ID="ID_932069906" CREATED="1658804127083" MODIFIED="1658809567195" HGAP_QUANTITY="17.75 pt" VSHIFT_QUANTITY="184.49999 pt">
+<hook URI="Stats_files/png_8965889282046282502.png" SIZE="0.6018054" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
 </node>
 </node>
 </map>
